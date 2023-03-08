c16bc89ca8f356136c5ad71dbfb132e249ccd122
Tom ROBERTO <tom.roberto31@gmail.com>
2023-03-08 12:10:27 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+          const response = await axios.post(
+            "https://site--cookies-server--vp8wfzxxmvxt.code.run/cookie",
+            {},
+            {
+              withCredentials: true,
+            }
+          );
+          console.log(response.data);
+        }}
+      >
+        <input type="submit" />
+      </form>
     </div>
   );
 }
